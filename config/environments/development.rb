76a2154a59718bb9a427a44a1b26cf90d0a0caf4
nzubariev <nick.zubariev@gmail.com>
2015-11-10 21:00:16 +0200

    My settings for mailing with SandGrid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = 'dry-hamlet-1899.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
       :address        => 'smtp.sendgrid.net',
